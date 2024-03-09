27e522739c40be7b7786a17d79953b8bf50af810
Kavinda Bandara <52319671+778569@users.noreply.github.com>
2024-03-09 10:24:45 +0530

    Add files via upload

---
+++ Scale and rotate images.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+import cv2
+
+img = cv2.imread("players.jpg", 1)
+
+#Scale
+
+#Convert image half of size
+
+img_half = cv2.resize(img,(0,0),fx=0.5,fy=0.5)
+
+img_stretch = cv2.resize(img,(600,600))
+
+img_stretch_near = cv2.resize(img,(600,600), interpolation=cv2.INTER_NEAREST)
+
+cv2.imshow("Half",img_half)
+cv2.imshow("Stretch",img_stretch)
+cv2.imshow("Stretch near",img_stretch_near)
+
+#Rotation
+
+M =cv2.getRotationMatrix2D((0,0),-30,1)
+rotated = cv2.warpAffine(img,M,(img.shape[1], img.shape[0]))
+
+cv2.imshow("Rotate",rotated)
+
+cv2.waitKeyEx()
+cv2.destroyAllWindows()
